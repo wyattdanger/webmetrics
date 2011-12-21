cea6e9f4b2c57a786ea76c849525831831f9da5e
Stephen Wyatt Bush <stephen.wyatt@gmail.com>
2011-12-21 14:28:51 -0500

    change API to request

--- lib/webmetrics.rb
+++ lib/webmetrics.rb
@@ -1,7 +1,48 @@
+require 'digest/sha1'
 
 module Webmetrics
-  class API
-    def initialize
+
+  class Request
+
+    def initialize(options = {})
+      @errors = []
+      defaults = {
+        format: 'json'
+      }
+      %w{method api_key username}.each do |req|
+        unless options.keys.include? req.intern
+          @errors.push "#{req} is required."
+        end
+      end
+
+      @options = defaults.merge options
+
     end
+
+    def query_string
+      params = ''
+      stack = []
+
+      @options.each do |k, v|
+        params << "#{k}=#{v}&"
+      end
+
+      params.chomp
+    end
+
+    def valid?
+      !@errors.any?
+    end
+
+    private # ----------------------------
+
+    def create_hash
+      ''
+    end
+
+    def create_signature
+      ''
+    end
+
   end
 end
--- test/test_webmetrics.rb
+++ test/test_webmetrics.rb
@@ -1,18 +1,43 @@
-require 'test/unit'
+require 'minitest/spec'
+require 'minitest/autorun'
 require_relative '../lib/webmetrics'
 
-class MyUnitTests < Test::Unit::TestCase
-
-  def setup
-    @w = Webmetrics::API.new
+describe Webmetrics::Request do
+  before do
+    @w = Webmetrics::Request.new(
+      api_key: '83c9bfe31801e45713a96c5c7f7b9a8aa0e04eec',
+      method:  'processeddata.getdata',
+      username: 'mailchimp'
+    )
   end
 
-  def teardown
-    ## Nothing
+  describe 'validations' do
+
+    it "is not valid without required options" do
+      Webmetrics::Request.new.valid?.must_equal false
+    end
+
+    it "is valid with required options" do
+      @w.valid?.must_equal true
+    end
   end
 
-  def test_existence
-    assert_not_nil @w
+  describe "query_string" do
+    it "returns a string" do
+      @w.query_string.class.must_equal String
+    end
+  end
+
+  describe "create_hash" do
+    it "creates a hash and adds it to options" do
+      skip "Implement in call()"
+    end
+  end
+
+  describe "create_signature" do
+    it "creates a signature and adds it to options" do
+      skip "Implement in call()"
+    end
   end
 
 end
