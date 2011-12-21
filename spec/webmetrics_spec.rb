718591c106170f57f2b82f93bbbea9bb9eafced2
Stephen Wyatt Bush <stephen.wyatt@gmail.com>
2011-12-21 16:34:51 -0500

    switch from test to spec in minitest

---
+++ spec/webmetrics_spec.rb
@@ -0,0 +1,36 @@
+require 'minitest/spec'
+require 'minitest/autorun'
+require 'yaml'
+require_relative 'spec_helper'
+require_relative '../lib/webmetrics'
+
+# place your credentials in a yaml file in spec for testing
+test_credentials = YAML.load_file File.join(
+  File.dirname(__FILE__), 'credentials.yml')
+
+describe Webmetrics::API do
+  before do
+    @w = Webmetrics::API.new(
+      api_key: test_credentials['api_key'],
+      username: test_credentials['username']
+    )
+  end
+
+  describe "get" do
+    it "makes an http request" do
+      @w.request :method => 'maintenance.getServices' do |res|
+        puts res.string
+        puts res.content_type
+      end
+    end
+
+    it "makes an http request" do
+      @w.request :method => 'maintenance.getServices', :format => 'json' do |res|
+        puts res.string
+        puts res.content_type
+      end
+    end
+
+  end
+
+end
