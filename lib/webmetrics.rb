f02728c93591e8088c72b2a3021dc0e3c82dc5c5
Stephen Wyatt Bush <stephen.wyatt@gmail.com>
2011-12-21 16:35:04 -0500

    implement simple api

--- lib/webmetrics.rb
+++ lib/webmetrics.rb
@@ -1,47 +1,70 @@
+require 'rubygems'
+require 'cgi'
 require 'digest/sha1'
+require 'base64'
+require 'open-uri'
+
 
 module Webmetrics
 
-  class Request
+  class API
+
+    attr_accessor :options
+
+    BASE_URL = "https://api.webmetrics.com/v2/"
 
     def initialize(options = {})
-      @errors = []
-      defaults = {
-        format: 'json'
-      }
-      %w{method api_key username}.each do |req|
-        unless options.keys.include? req.intern
-          @errors.push "#{req} is required."
-        end
-      end
+      @options = options
 
-      @options = defaults.merge options
+      requires :api_key
+      requires :username
 
     end
 
-    def query_string
-      params = ''
+    def request (options = {})
+
+      query_string = build_query_string(options)
+
+      open "#{BASE_URL}#{query_string}" do |response|
+        yield response
+      end
+
+    end
+
+    private 
+
+    def build_query_string(options = {})
+      params = '?'
       stack = []
 
-      @options.each do |k, v|
+      options[:username] = @options[:username]
+      options[:sig]      = signature
+
+      options.each do |k, v|
         params << "#{k}=#{v}&"
       end
 
-      params.chomp
+      params.chop
     end
 
-    def valid?
-      !@errors.any?
+    def timestamp
+      Time.new().to_i
     end
 
-    private # ----------------------------
-
-    def create_hash
-      ''
+    def sha1
+      Digest::SHA1.digest("#{options[:username]}#{options[:api_key]}#{timestamp}")
     end
 
-    def create_signature
-      ''
+    def b64
+      Base64.encode64(sha1.to_s.strip).gsub("\n", '')
+    end
+
+    def signature
+      CGI.escape b64
+    end
+
+    def requires(param)
+      raise "#{ param } is REQUIRED" unless options.keys.include? param 
     end
 
   end
