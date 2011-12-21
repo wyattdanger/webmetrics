65902b430788601b7223859e64dd93a13cdcc955
Stephen Wyatt Bush <stephen.wyatt@gmail.com>
2011-12-21 17:07:04 -0500

    better response test

--- spec/webmetrics_spec.rb
+++ spec/webmetrics_spec.rb
@@ -5,25 +5,22 @@ require_relative 'spec_helper'
 require_relative '../lib/webmetrics'
 
 # place your credentials in a yaml file in spec for testing
-test_credentials = YAML.load_file File.join(
-  File.dirname(__FILE__), 'credentials.yml')
+test_credentials = YAML.load_file(File.join(File.dirname(__FILE__), 'credentials.yml'))
 
 describe Webmetrics::API do
   before do
-    @w = Webmetrics::API.new(
+    @wm = Webmetrics::API.new(
       api_key: test_credentials['api_key'],
       username: test_credentials['username']
     )
   end
 
   describe "get" do
-    it "makes an http request" do
-      @w.request :method => 'maintenance.getServices' do |res, io|
-        puts res
-        puts io.content_type
+    it "yields a Hash and a StringIO" do
+      @wm.request :method => 'maintenance.getServices' do |res, io|
+        res.must_be_instance_of Hash
+        io.must_be_instance_of  StringIO
       end
     end
-
   end
-
 end
